094e7132abc589833c46a8dd19244223336fe31e
Sucuri Security <guara.taste@gmail.com>
2026-07-01 15:10:16 -0300

    adicionado caracter Literal no TicketUpdate (main backend)

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import json
 import os
 import sqlite3
-from typing import Dict, Optional
+from typing import Dict, Literal, Optional
 
 import uvicorn
 from fastapi import FastAPI, HTTPException
@@ -23,8 +23,8 @@ DB_PATH = "db.sqlite"
 
 # Schemas do Pydantic (Tipagem)
 class TicketUpdate(BaseModel):
-    status: Optional[str] = None
-    priority: Optional[str] = None
+    status: Optional[Literal["open", "pending", "closed"]] = None
+    priority: Optional[Literal["low", "normal", "high"]] = None
 
 
 class MetricsResponse(BaseModel):
@@ -114,8 +114,6 @@ def update_ticket(ticket_id: int, ticket: TicketUpdate):
         raise HTTPException(status_code=404, detail="Ticket not found")
 
     updated_data = {"id": row[0], "status": row[4], "priority": row[5]}
-
-    # O bloco do n8n foi totalmente removido daqui para focar apenas no retorno limpo dos dados.
 
     return {"message": "Ticket updated", "data": updated_data}
 
